185aa6bb0db69e0dfaa3bda096b36f6167955519
nikit <nikitagabry523@gmail.com>
2022-01-22 00:09:12 +0200

    finish taask

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+const indexRouter = require('./routes/index');
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+module.exports = app;
